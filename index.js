c886566d30adec1235f7e1037e7b52d033b92ab1
FireRipper <shabelnyk.ilya@gmail.com>
2020-04-16 18:00:56 +0300

    feat: add script for running server and client together. And to index.js app.use(express.json({ extended: true })) - for correct getting data from client.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const authRoutes = require('./routes/auth.routes')
 
 const app = express()
 
+app.use(express.json({ extended: true }))
+
 app.use('/api/auth', authRoutes)
 
 const PORT = config.get('port') || 2000
@@ -13,7 +15,8 @@ async function start() {
     try {
         await mongoose.connect(config.get('mongoUrl'), {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useCreateIndex: true
         })
 
         app.listen(PORT, () => console.log(`App has been started on port: ${PORT}`))
