477ce830016c236a3673d1e66d1477b029f4c282
FireRipper <shabelnyk.ilya@gmail.com>
2020-04-16 14:22:46 +0300

    feat(login user): add package - jsonwebtocken for creating unique token. Add logic for process login user (validation, check user in db and login him).

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express')
 const bcrypt = require('bcryptjs')
-const {check, validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
 const User = require('../models/User')
 const router = Router()
 
@@ -12,39 +14,79 @@ router.post('/register',
             .isLength({ min: 6 })
     ],
     async (req, res) => {
-    try {
-        const errors = validationResult(req)
+        try {
+            const errors = validationResult(req)
 
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array(),
-                message: 'Некорректный данные при регистрации'
-            })
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Некорректный данные при регистрации'
+                })
+            }
+
+            const { email, password } = req.body
+
+            const candidate = await User.findOne({ email })
+
+            if (candidate) {
+                return res.status(400).json({ message: 'Такой пользователь уже существует' })
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12)
+            const user = new User({ email, password: hashedPassword })
+
+            await user.save()
+
+            res.status(201).json({ message: 'Пользователь создан' })
+
+        } catch (e) {
+            res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
         }
-
-        const { email, password } = req.body
-
-        const candidate = await User.findOne({ email })
-
-        if (candidate) {
-            return res.status(400).json({ message: 'Такой пользователь уже существует' })
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 12)
-        const user = new User({ email, password: hashedPassword })
-
-        await user.save()
-
-        res.status(201).json({ message: 'Пользователь создан' })
-
-    } catch (e) {
-        res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
-    }
-})
+    })
 
 // /api/auth/login
-router.post('/login', async (req, res) => {
+router.post('/login', [
+        check('email', 'Введите корректный email').normalizeEmail().isEmail(),
+        check('password', 'Введите пароль').exists()
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
 
-})
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Некорректный данные при входу в систему'
+                })
+            }
+
+            const { email, password } = req.body
+
+            // Check user in DB
+            const user = await User.findOne({ email })
+
+            if (!user) {
+                return res.status(400).json({ message: 'Пользователь не найден' })
+            }
+
+            // Check hashes passwords
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if (!isMatch) {
+                return res.status(400).json({ message: 'Введите корректные данные' })
+            }
+
+            const token = jwt.sign(
+                { user: user.id },
+                config.get('jwtSecret'),
+                { expiresIn: '1h' }
+            )
+
+            res.json({ token, userId: user.id })
+
+        } catch (e) {
+            res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+        }
+    })
 
 module.exports = router
